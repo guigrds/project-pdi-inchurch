c797d628c7186c34e712d2fe9d85899a4bb5d521
guigrds <guilherme.rodrigues@inchurch.com.br>
2022-12-01 16:03:05 -0300

    Create app file

---
+++ PDI inChurch iOS Programming/PDI inChurch iOS Programming/PDI_inChurch_iOS_ProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDI_inChurch_iOS_ProgrammingApp.swift
+//  PDI inChurch iOS Programming
+//
+//  Created by Guilherme Rodrigues on 01/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PDI_inChurch_iOS_ProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
