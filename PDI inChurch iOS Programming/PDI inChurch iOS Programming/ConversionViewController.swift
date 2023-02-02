8fe487b50c206af1fa61967ff1ff494bd659255d
guigrds <guilherme.rodrigues@inchurch.com.br>
2023-02-02 11:58:04 -0300

    Atualizando projeto

---
+++ PDI inChurch iOS Programming/PDI inChurch iOS Programming/ConversionViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ConversionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController loaded its view.")
+    }
+}
--- PDI inChurch iOS Programming/PDI inChurch iOS Programming/MapViewController.swift
+++ PDI inChurch iOS Programming/PDI inChurch iOS Programming/MapViewController.swift
@@ -6,11 +6,53 @@
 //
 
 import UIKit
+import MapKit
 
 class mapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard","Hybrid","Satellite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self,action: #selector(mapTypeChanged(_:)),for: .valueChanged)
+
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+//        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+//        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
     
 }
